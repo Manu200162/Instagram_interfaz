b2f25699e56c0810cf2ea428eaa2ac60d3bc9b00
Manu200162 <manli200134@gmail.com>
2020-06-08 19:04:09 -0400

    First Commit

---
+++ app/src/main/java/com/manuelvalenzuelajr/instagram_interfaz/MainActivity.java
@@ -0,0 +1,14 @@
+package com.manuelvalenzuelajr.instagram_interfaz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
